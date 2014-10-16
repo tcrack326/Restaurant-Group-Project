bb8c5dd0d02927e8e1b6fd0ccfc5f798617f9396
Paul Park <ppark482@gmail.com>
2014-10-16 07:56:53 -0400

    added more javascript

--- app/scripts/paul.js
+++ app/scripts/paul.js
@@ -1,8 +1,10 @@
 var menuURL = "http://restaurantapi.apiary.io/menu";
 
+// Start of Templating of Appetizers
 var apps_Template = $('#menuApps').html;
     apps_Render = _.template(apps_Template);
 
+// Injecting Appertizers
 $.getJSON(menuURL).done( function(menu_data) {
   menu_data.appetizers.forEach( function (apps_data) {
     $('.menuItems').html(apps_Render(apps_data));
