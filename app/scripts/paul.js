963b8d08e6d76807a66add03147cf2eecd24f1f2
Paul Park <ppark482@gmail.com>
2014-10-16 18:49:40 -0400

    daily special javascript doesn't work

--- app/scripts/paul.js
+++ app/scripts/paul.js
@@ -1,25 +1,6 @@
-var menuURL = "http://restaurantapi.apiary.io/menu";
-    latestURL = "http://restaurantapi.apiary.io/news/latest";
-    specialURL = "http://restaurantapi.apiary.io/menu/special"
-
-
-// Start Templating of Updating News Boxes
-var latest_Template = $('#newsTemplate').html();
-    latest_Render = _.template(latest_Template);
-    special_Template = $('#specialTemplate').html();
-    special_Render = _.template(special_Template);
-
-$.getJSON(latestURL).done( function(news) {
-
-  $('.updatingNews').html(special_Render(news));
-});
-
-
-
-
-
-
-
+var menuURL = "http://restaurantapi.apiary.io/menu",
+    latestURL = "http://restaurantapi.apiary.io/news/latest",
+    specialURL = "http://restaurantapi.apiary.io/menu/special";
 
 
 // Start of Templating of Appetizers
@@ -40,5 +21,43 @@ $.getJSON(menuURL).done( function(menu_data) {
   });
   menu_data.sides.forEach( function (sides_data) { // Sides
     $('.itemsMenu  .sidesMenu').append(sides_Render(sides_data));
+  });
+});
+
+
+// Start Templating of Updating News Boxes
+var latest_Template = $('#newsTemplate').html();
+    latest_Render = _.template(latest_Template);
+    special_Template = $('#specialTemplate').html();
+    special_Render = _.template(special_Template);
+
+// Updating News feed
+$.getJSON(latestURL).done( function(news) {
+  $('.updatingNews').append(latest_Render(news));
+});
+
+// Updating Special
+var menuItemId;
+
+$.getJSON(specialURL).done( function(special) {
+  // determines id of special of day from menu
+  var menuItemId = special.menu_item_id;
+});
+
+$.getJSON(menuURL).done( function(menu_item) {
+  menu_item.appetizers.forEach( function(apps_data) { // loops through appetizers to look for id
+    if (apps_data.id === menuItemId) { // if found use that appetizers
+      $('.updatingSpecial').append(special_Render(apps_data));
+    };
+  });
+  menu_item.entrees.forEach( function(entree_data) { // loops through entrees to look for id
+    if (entree_data.id === menuItemId) { // if found use that entree
+      $('.updatingSpecial').append(special_Render(entree_data));
+    };
+  });
+  menu_item.sides.forEach( function(sides_data) { // loops through sides to look for id
+    if (sides_data.id === menuItemId) { // if found use that side
+      $('.updatingSpecial').append(special_Render(sides_data));
+    };
   });
 });
